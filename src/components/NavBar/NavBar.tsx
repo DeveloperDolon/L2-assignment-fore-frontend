73da01e5d092dceac523c1b6a7138a01d75178a9
Dolon Chandra Roy <dolonr718@gmail.com>
2024-08-07 23:07:42 +0600

    navbar layout and react icons added commit

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,27 +1,38 @@
 import MyContainer from "../MyContainer/MyContainer";
 import logoImage from "../../assets/logo.png";
 import { NavLink } from "react-router-dom";
+import { IoCart } from "react-icons/io5";
 
 const NavBar = () => {
   return (
     <div>
-      <MyContainer className="bg-red-50 flex justify-between">
+      <MyContainer className="flex justify-between md:py-3 sm:py-2 py-1">
         <div>
-          <img src={logoImage} alt="logo images" />
+          <img
+            className="md:w-20 sm:w-14 w-10 md:h-12 sm:h-10 h-8"
+            src={logoImage}
+            alt="logo images"
+          />
         </div>
 
         <div>
-          <ul>
+          <ul className="flex md:gap-4 gap-3 md:text-base sm:text-sm text-xs">
             <li>
               <NavLink to={"/"}>Home</NavLink>
             </li>
+
             <li>
               <NavLink to={"/products"}>Products</NavLink>
             </li>
+
             <li>
               <NavLink to={"/about"}>About Us</NavLink>
             </li>
           </ul>
+        </div>
+
+        <div>
+          <IoCart />
         </div>
       </MyContainer>
     </div>
