f61fff313c806aa7b8049d1686470c6bdfde011e
Dolon Chandra Roy <dolonr718@gmail.com>
2024-08-07 21:37:39 +0600

    redux toolkit setup complete

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { MainRoute } from "./router/MainRouter";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={MainRoute} />
+    <Provider store={store}>
+      <RouterProvider router={MainRoute} />
+    </Provider>
   </React.StrictMode>
 );
