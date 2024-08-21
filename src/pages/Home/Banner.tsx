922b03ef4c9802aa1a45f1e0b168714c0a16493d
Dolon Chandra Roy <dolonr718@gmail.com>
2024-08-21 20:56:18 +0600

    fontsetup commit

--- src/pages/Home/Banner.tsx
+++ src/pages/Home/Banner.tsx
@@ -1,14 +1,21 @@
 import bannerImage from '@/assets/bgImage.jpg';
+import MyContainer from '@/components/MyContainer/MyContainer';
 
 const Banner = () => {
   return (
     <div
       style={{
-        background: `url(${bannerImage}) no-repeat center center`,
+        background: `linear-gradient(black, white), url(${bannerImage}) no-repeat center center`,
         backgroundSize: 'cover',
       }}
-      className='min-h-screen bg-red-300'
-    ></div>
+      className='bg-red-300'
+    >
+      <MyContainer className='min-h-screen flex items-center justify-center'>
+        <h1 className='md:text-6xl sm:text-4xl text-3xl text-center font-bold text-white font-secondary'>
+          Welcome to Our Online Store!
+        </h1>
+      </MyContainer>
+    </div>
   );
 };
 
