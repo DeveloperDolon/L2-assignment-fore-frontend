e316a956270f1bbe866408de963b3f2a61ab603e
DeveloperDolon <dolonr718@gmail.com>
2024-10-19 22:30:12 +0600

    more type added on type file

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,9 +1,10 @@
 type SliderImagesType = {
   caption: string;
-  image: string;
+  image: string | File;
 };
 
 type Product = {
+  id?: string;
   actualPrice: number;
   name: string;
   discountPrice: number;
@@ -13,8 +14,43 @@ type Product = {
   inStock: boolean;
   quantity: number;
   categoryId: string;
-  reviewsCount?: number;
+  brandId: string;
   averageRating: number;
+  reviewCount: number;
   warranty: string;
   policies: string;
+};
+
+type Review = {
+  id?: string;
+  userId: string;
+  productId: string;
+  reviewMessage: string;
+  rating: number;
+  isVerified: boolean;
+  likeCount: number;
+  dislikeCount: number;
+  createdAt: Date;
+};
+
+type Brand = {
+  id?: string;
+  brandImage: string | File;
+  name: string;
+};
+
+type Category = {
+  id?: string;
+  name: string;
+  image: string | File;
+};
+
+type User = {
+  name: string;
+  email: string;
+  password: string;
+  phone?: string;
+  address?: string;
+  role?: string;
+  avatar?: string | File;
 };
