ae19c7c1ac948fd8ee9ffbe0b9ff73384abe5209
DeveloperDolon <dolonr718@gmail.com>
2024-10-30 00:03:43 +0600

    gallery section design

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import Banner from './Banner';
 import BenefitsSection from './BenefitsSection';
 import Categories from './Categories';
 import FeaturedProducts from './FeaturedProducts';
+import ImageGallery from './ImageGallery';
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Categories />
       <FeaturedProducts />
       <BenefitsSection />
+      <ImageGallery />
     </>
   );
 };
