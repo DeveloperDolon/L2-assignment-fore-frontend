7cebdd7f52cfcb83d4211563fe93f689c61cabfa
DeveloperDolon <dolonr718@gmail.com>
2024-11-25 20:57:39 +0600

    product form constants added

--- src/constant/formFields.ts
+++ src/constant/formFields.ts
@@ -1,17 +1,117 @@
-type DynamicFormFieldType = 'text' | 'textarea' | 'select' | 'date';
+type DynamicFormFieldType = 'checkbox' | 'text' | 'textarea' | 'select' | 'date' | 'number' | 'file';
 
 export type DynamicFormField = {
   type : DynamicFormFieldType;
   name: string;
   label?: string;
-  defaultValue?: string;
+  defaultValue?: string | number;
   placeholder?: string;
   required?: boolean;
   errorMessage?: string;
-  options: {value?: string | number; label: string;}
+  options?: {value?: string | number; label: string;}[];
 };
 
-
-export const addProductFormFields : Product = {
-    
-};
+export const addProductFormFields : DynamicFormField[] = [
+    {
+        type: 'text',
+        name: 'product_name',
+        label: 'Product Name',
+        placeholder: 'Input product name',
+        required: true,
+        errorMessage: 'Product name must need to input!',
+    },
+    {
+        type: 'number',
+        name: 'actual_price',
+        label: 'Actual Price',
+        placeholder: 'Input product actual price',
+        required: true,
+        errorMessage: 'Product actual price need to input!'
+    },
+    {
+        type: 'number',
+        name: 'discount_price',
+        label: 'Discount price',
+        placeholder: 'Input product discount price',
+        required: true,
+        errorMessage: 'Product discount price must need to input.'
+    },
+    {
+        type: 'file',
+        name: 'slider_images',
+        label: 'Slider images',
+        placeholder: 'Input images',
+        required: true,
+        errorMessage: 'Product image must need to upload.'
+    },
+    {
+        type: 'number',
+        name: 'discount',
+        label: 'Discount',
+        defaultValue: 0,
+        placeholder: 'Enter discount - (Percentage) ',
+        required: false
+    },
+    {
+        type: 'text',
+        name: 'discription',
+        label: 'Discription',
+        placeholder: "Enter descripition",
+        required: true,
+        errorMessage: 'Product description must need.'
+    },
+    {
+        type: 'checkbox',
+        name: 'in_stock',
+        label: 'In Stock',
+        required: true,
+    },
+    {
+        type: 'number',
+        name: 'quantity',
+        label: 'Quantity',
+        required: true,
+        placeholder: 'Input quantity',
+        errorMessage: 'Quantity must need to input.'
+    },
+    {
+        type: 'select',
+        name: 'categoryId',
+        label: 'Category',
+        placeholder: 'Select category',
+        required: true,
+        options: [
+            {
+                label: 'Xyz',
+                value: 'xyz'
+            }
+        ],
+        errorMessage: 'Must need to select category.'
+    },
+    {
+        type: 'select',
+        name: 'brandId',
+        label: 'Brand',
+        placeholder: 'Select brand',
+        required: true,
+        options: [
+            {
+                label: 'Xyz',
+                value: 'xyz'
+            }
+        ],
+        errorMessage: 'Must need to select brand.'
+    },
+    {
+        type: 'text',
+        name: 'warranty',
+        label: 'Warrranty',
+        placeholder: 'Input warranty.'
+    },
+    {
+        type: 'text',
+        name: 'policies',
+        label: 'Policies',
+        placeholder: 'Input Policies.',
+    }
+];
