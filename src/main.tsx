ee18353588d483c06681f41b62a084a4d55485fc
DeveloperDolon <dolonr718@gmail.com>
2024-12-29 22:51:31 +0600

    redux persist configuration added

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { MainRoute } from "./router/MainRouter";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { MainRoute } from './router/MainRouter';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={MainRoute} />
+      <PersistGate
+        loading={null}
+        persistor={persistor}
+      >
+        <RouterProvider router={MainRoute} />
+      </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
